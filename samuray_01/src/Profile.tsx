29a64dde4ff07fb40063b42d10a123a1acc612a7
kseniia <ksusha.mamatova98@gmail.com>
2022-08-24 13:52:55 +0300

    git init

---
+++ samuray_01/src/Profile.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src='https://loveincorporated.blob.core.windows.net/contentimages/gallery/b7b8be87-6ae1-4a97-af8e-6349165e456e-2-alberobello.jpg'/>
+            </div>
+            <div>ava</div>
+            <div>My posts</div>
+            <div>New posts</div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+            <div></div>
+        </div>
+    )
+}
+
+export {Profile}
